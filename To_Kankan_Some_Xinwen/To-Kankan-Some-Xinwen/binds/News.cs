15b9de3f2afb9963da9ca5fe43c53006c51a7dc7
lugf027 <46019075+lugf027@users.noreply.github.com>
2019-11-05 10:51:59 +0800

    整理目录，连接

---
+++ To_Kankan_Some_Xinwen/To-Kankan-Some-Xinwen/binds/News.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using To_Kankan_Some_Xinwen.news;
+
+namespace To_Kankan_Some_Xinwen.binds
+{
+    class News
+    {
+
+        public static RenminwangNews[] GetRenminwangNews(string type, string page)
+        {
+            string data = "type="+type+"&page="+page;  //要上传到网页系统里的数据（字段名=数值 ，用&符号连接起来）
+            byte[] bytesToPost = System.Text.Encoding.Default.GetBytes(data); //转换为bytes数据
+
+            string responseResult = String.Empty;
+            HttpWebRequest req = (HttpWebRequest)
+            HttpWebRequest.Create("http://localhost:8080/renminwang");   //创建一个有效的httprequest请求，地址和端口和指定路径必须要和网页系统工程师确认正确，不然一直通讯不成功
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded;charset=gb2312";
+            req.ContentLength = bytesToPost.Length;
+
+            using (Stream reqStream = req.GetRequestStream())
+            {
+                reqStream.Write(bytesToPost, 0, bytesToPost.Length);     //把要上传网页系统的数据通过post发送
+            }
+            HttpWebResponse httpWebResponse = (HttpWebResponse)req.GetResponse();
+
+            RenminwangNews[] result;
+
+            if (httpWebResponse != null && httpWebResponse.StatusCode == HttpStatusCode.OK)
+            {
+                StreamReader sr;
+                using (sr = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    responseResult = sr.ReadToEnd();  //网页系统的json格式的返回值，在responseResult里，具体内容就是网页系统负责工程师跟你协议号的返回值协议内容
+                    result = JsonConvert.DeserializeObject<RenminwangNews[]>(responseResult);
+                }
+                sr.Close();
+            }
+            else
+            {
+                return null;
+            }
+            httpWebResponse.Close();
+            return result;
+        }
+    }
+}
---
+++ To_Kankan_Some_Xinwen/To-Kankan-Some-Xinwen/forms/Form_News.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Smobiler.Core;
+using Smobiler.Core.Controls;
+using To_Kankan_Some_Xinwen.binds;
+using To_Kankan_Some_Xinwen.news;
+
+namespace To_Kankan_Some_Xinwen
+{
+    partial class Form_Defult : Smobiler.Core.Controls.MobileForm
+    {
+        public Form_Defult() : base()
+        {
+            //This call is required by the SmobilerForm.
+            InitializeComponent();
+        }
+
+        private void Form_Defult_Load(object sender, EventArgs e)
+        {
+            //首页新闻
+            RenminwangNews[] renminwangNews = News.GetRenminwangNews(Config.defaultNewsType, Config.defaultNewsPage);
+
+            DataTable table = new DataTable();
+            table.Columns.Add("NEWS_TITLE", typeof(System.String));
+            table.Columns.Add("NEWS_SOURCE", typeof(System.String));
+            table.Columns.Add("NEWS_TIME", typeof(System.String));
+
+            for(int i=0; i<renminwangNews.Length; i++)
+            {
+                table.Rows.Add(renminwangNews[i].title, renminwangNews[i].source, renminwangNews[i].time);
+
+            }
+
+            listView1.Rows.Clear(); //清除数据
+            if(table.Rows.Count > 0)    //绑定数据源
+            {
+                listView1.DataSource = table;
+                listView1.DataBind();
+            }
+        }
+
+        private void title_defult_ImagePress(object sender, EventArgs e)
+        {
+            OpenDrawer();
+        }
+
+        private void toolBar1_ToolbarItemClick(object sender, ToolbarClickEventArgs e)
+        {
+            switch (e.Name)
+            {
+                case "Hots": Form_Hots form_hots = new Form_Hots();this.Show(form_hots); break;
+                case "News":Form_Defult form_news = new Form_Defult(); this.Show(form_news); break;
+                case "Types": Form_Types form_types = new Form_Types(); this.Show(form_types); break;
+                case "Mine": Form_Mine form_mine = new Form_Mine(); this.Show(form_mine); break;
+                default:
+                    break;
+            };
+
+        }
+    }
+}
